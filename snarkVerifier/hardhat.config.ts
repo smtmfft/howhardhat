faa421a504444d2c90d72a5d518f8f685d87a3fc
john xu <john@taiko.xyz>
2023-02-24 14:28:50 +0800

    fix: reduce contract size

--- snarkVerifier/hardhat.config.ts
+++ snarkVerifier/hardhat.config.ts
@@ -6,12 +6,6 @@ const config: HardhatUserConfig = {
 	solidity: "0.8.9",
 	allowJs: true,
 	noImplicitAny: false,
-	networks: {
-		hardhat: {
-			//blockGasLimit: 0x1fffffffffffff,
-			allowUnlimitedContractSize: true,
-		},
-	},
 };
 
 export default config;
